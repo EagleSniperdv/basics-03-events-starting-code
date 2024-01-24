30c03065eead1f6547860e4b042b517bfb9140b4
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-01-24 11:58:55 +0300

    binding evets and data

--- app.js
+++ app.js
@@ -1,9 +1,57 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 4,
+      prof: '',
+      confirmedProf:''
     };
   },
+
+  computed: {
+
+    fullprof() {
+      if (this.prof === '') {
+        return '';
+      } else {
+        return this.prof +' ' + 'Nyagoko';
+      }
+    }
+  },
+
+  methods: {
+
+    fullProf() {
+      if (this.prof === '') {
+        return '';
+      } else {
+        return this.prof +' ' + 'Nyagoko';
+      }
+    },
+
+    resetProf() {
+      this.prof = '';
+    },
+
+    confirmProf() {
+      this.confirmedProf = this.prof;
+    },
+
+    subForm() {
+      alert('submitted');
+    },
+
+    setProf(event) {
+      this.prof = event.target.value;
+    },
+
+    add(num) {
+      this.counter = this.counter + num;
+    },
+
+    remove(num) {
+      this.counter = this.counter - num;
+    } 
+  }
 });
 
 app.mount('#events');
